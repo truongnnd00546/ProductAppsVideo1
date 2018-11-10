07306284fd203ffbd4489b53f06d261d8175081b
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-11-10 09:35:09 +0700

    Success

---
+++ ProductApps/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ProductApps.Models;
+
+namespace ProductApps.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
